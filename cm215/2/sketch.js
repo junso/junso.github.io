587bd57431b33a35006126938f7f0c3c59a93b32
junso <jsfofana@gmail.com>
2016-10-05 11:16:29 -0500

    t

--- cm215/2/sketch.js
+++ cm215/2/sketch.js
@@ -17,6 +17,7 @@ function draw() {
   for (var i = 0; i < touches.length; i++) {
     rect(touches[i].x, touches[i].y, 150, 150);
 
+/*
     if ((i + 1) < touches.length) {
 
       line(touches[i].x,touches[i].y,touches[i + 1].x,touches[i + 1].y);
@@ -24,7 +25,7 @@ function draw() {
 
       line(touches[i].x, touches[i].y, touches[0].x, touches[0].y);
     }
-
+*/
   }
 }
   function touchStarted() {}
