f3f00da3bc103608c54bb545499889052020a689
junso <jsfofana@gmail.com>
2016-10-05 09:54:48 -0500

    t

--- cm215/2/sketch.js
+++ cm215/2/sketch.js
@@ -1,14 +1,16 @@
 function setup() {
   createCanvas(windowWidth,windowHeight);
   rectMode(CENTER);
-  fill(255,0,0);
+ stroke(0);
+ strokeWeight(5);
 }
 
 function draw() 
-{}
-
-  function touchStarted(){
+{
   background (255);
-    fill(random(0,255),(random(0,255),(random(0,255));
-  rect(text(touches[0].x,touches[0].y,150,150);
+  fill(random(0,255),(random(0,255),(random(0,255));
+    
+  for(var i=0; i<touches.length;i++){
+    rect(touches[i].x,touches[i].y,150,150);
+  }
 }
