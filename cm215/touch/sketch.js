4eb572be1af4417dd5c70e7cb9e48a31dd389e67
junso <jsfofana@gmail.com>
2016-10-19 09:38:08 -0500

    beginshape

--- cm215/touch/sketch.js
+++ cm215/touch/sketch.js
@@ -12,15 +12,16 @@ function setup() {
 
 function draw() {
   background(255);
-
+ fill(255);
   for (var i = 0; i < touches.length; i++) {
     ellipse(touches[i].x, touches[i].y, 80,80);
-
+   
   }
     fill(random(0, 255), random(0, 255), random(0, 255));
+    
   beginShape();
   for (var i = 0; i < touches.length; i++) {
-    
+  
     vertex(touches[i].x, touches[i].y);
   }
   endShape(CLOSE);
