28eb83744953a4fcc51f5456bff5fcd100209864
junso <jsfofana@gmail.com>
2016-10-19 09:30:37 -0500

    hghg

--- cm215/touch/sketch.js
+++ cm215/touch/sketch.js
@@ -7,21 +7,20 @@ function setup() {
   ellipseMode(CENTER);
   stroke(0);
   noFill();
-  strokeWeight(5);
+  strokeWeight(8);
 }
 
 function draw() {
   background(255);
 
   for (var i = 0; i < touches.length; i++) {
-
-    ellipse(touches[i].x, touches[i].y, 80);
-
-    beginShape();
-    for (var i = 0; i < touches.length; i++) {
-      vertex(touches[i].x, touches[i].y);
-
-      endShape(CLOSE);
-    }
+    ellipse(touches[i].x, touches[i].y, 80,80);
   }
+  
+  beginShape();
+  for (var i = 0; i < touches.length; i++) {
+    vertex(touches[i].x, touches[i].y);
+  }
+  endShape(CLOSE);
+
 }
