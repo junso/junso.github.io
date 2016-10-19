85b6e2523c6f3c18c6c8ff0e5ed96e090589839a
junso <jsfofana@gmail.com>
2016-10-19 09:14:55 -0500

    beginShape

--- cm215/touch/sketch.js
+++ cm215/touch/sketch.js
@@ -1,9 +1,9 @@
-document.ontouchmove = function(event){
+document.ontouchmove = function(event) {
   event.preventDefault();
 }
 
 function setup() {
-  createCanvas(windowWidth,windowHeight);
+  createCanvas(windowWidth, windowHeight);
   ellipseMode(CENTER);
   stroke(0);
   noFill();
@@ -12,49 +12,42 @@ function setup() {
 
 function draw() {
   background(255);
-  
-  for(var i=0;i<touches.length;i++){
-    
-    
-    if((i+1)<touches.length){
-     
+
+  for (var i = 0; i < touches.length; i++) {
+
+    if ((i + 1) < touches.length) {
+
       beginShape();
-   
- fill(random(0,255),random(0,255),random(0,255)); 
- 
-vertex(touches[i].x, touches[i].y);
 
-vertex(touches[i].x, touches[i].y);
+      vertex(touches[i].x, touches[i].y);
 
-vertex(touches[i+1].x, touches[i+1].y);
+      vertex(touches[i].x, touches[i].y);
 
-vertex(touches[i+1].x, touches[i+1].y);
-endShape(CLOSE);
-    
-    //line(touches[i].x,touches[i].y,touches[i+1].x,touches[i+1].y);
-    
-      
+      vertex(touches[i + 1].x, touches[i + 1].y);
+
+      vertex(touches[i + 1].x, touches[i + 1].y);
+
+      endShape(CLOSE);
+
+    } else {
+
+      beginShape();
+
+      vertex(touches[i].x, touches[i].y);
+
+      vertex(touches[i].x, touches[i].y);
+
+      vertex(touches[0].x, touches[0].y);
+
+      vertex(touches[0].x, touches[0].y);
+
+      endShape(CLOSE);
+
+
     }
-    
- else{
- 
-beginShape();
-//fill(255,0,0);
-vertex(touches[i].x, touches[i].y);
-
-vertex(touches[i].x, touches[i].y);
-
-vertex(touches[0].x, touches[0].y);
-
-vertex(touches[0].x, touches[0].y);
-endShape(CLOSE);
-   
-   // line(touches[i].x,touches[i].y,touches[0].x,touches[0].y);
-}
-
-   ellipse(touches[i].x,touches[i].y,75); 
+    ellipse(touches[i].x, touches[i].y, 75);
   }
 }
 
 //function touchStarted() {
-  //}
+//}
