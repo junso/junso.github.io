3d5fbfd70228407675d71a9fb8a83fdbde1e2784
junso <jsfofana@gmail.com>
2016-10-19 11:03:56 -0500

    drawBall

--- cm215/inclinaison/sketch.js
+++ cm215/inclinaison/sketch.js
@@ -1,13 +1,24 @@
 function setup() {
  createCanvas(windowWidth,windowHeight); 
+
+  ellipseMode(CENTER);
+  ballSize = 30 ;
 }
 
 function draw() {
   
+drawBall() {
+ 
  background(255);
  
  textSize(45);
+ 
  text("Rx: " + floor(rotationX), 100, 100);
  text("Ry: " + floor(rotationY), 100, 150);
  text("Rz: " + floor(rotationZ), 100, 200);
+}
+
+function drawBall(){
+  fill(100);
+  ellipse(windowHeight/2 , windowWidth/2, ballSize,ballSize);
 }
