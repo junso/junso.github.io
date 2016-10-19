2175d37dad7e419c66d9d3799535b44b58bdfbac
junso <jsfofana@gmail.com>
2016-10-19 09:21:17 -0500

    bb

--- cm215/touch/sketch.js
+++ cm215/touch/sketch.js
@@ -13,36 +13,37 @@ function setup() {
 function draw() {
   background(255);
 
+ 
 
+    if ((i + 1) < touches.length) {
 
-  if ((i + 1) < touches.length) {
-
-    beginShape();
-    for (var i = 0; i < touches.length; i++) {
+      beginShape();
+ for (var i = 0; i < touches.length; i++) {
       vertex(touches[i].x, touches[i].y);
-    }
-    endShape(CLOSE);
-
-  } else {
-
-    beginShape();
-    
-    for (var i = 0; i < touches.length; i++) {
-
-      vertex(touches[i].x, touches[i].y);
-
-      vertex(touches[i].x, touches[i].y);
-
-      vertex(touches[0].x, touches[0].y);
-
-      vertex(touches[0].x, touches[0].y);
-    }
-    endShape(CLOSE);
-  }
-
-  for (var i = 0; i < touches.length; i++) {
-
-    ellipse(touches[i].x, touches[i].y, 80);
-  }
+      
+      vertex(touches[i + 1].x, touches[i + 1].y);
 }
+      endShape(CLOSE);
+
+    } else {
+
+      beginShape();
+
+      vertex(touches[i].x, touches[i].y);
+
+      vertex(touches[i].x, touches[i].y);
+
+      vertex(touches[0].x, touches[0].y);
+
+      vertex(touches[0].x, touches[0].y);
+
+      endShape(CLOSE);
+    }
+
+   
+
+      ellipse(touches[i].x, touches[i].y, 80);
+}
+
+//function touchStarted() {
 //}
