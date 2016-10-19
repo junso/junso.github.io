b4de6b6be2fbaf44c99581357adc129a903bcdf1
junso <jsfofana@gmail.com>
2016-10-19 09:11:52 -0500

    beginShape

--- cm215/touch/sketch.js
+++ cm215/touch/sketch.js
@@ -19,7 +19,9 @@ function draw() {
     if((i+1)<touches.length){
      
       beginShape();
-   // fill(255,0,0);
+   
+ fill(random(0,255),random(0,255),random(0,255)); 
+ 
 vertex(touches[i].x, touches[i].y);
 
 vertex(touches[i].x, touches[i].y);
@@ -28,8 +30,12 @@ vertex(touches[i+1].x, touches[i+1].y);
 
 vertex(touches[i+1].x, touches[i+1].y);
 endShape(CLOSE);
+    
     //line(touches[i].x,touches[i].y,touches[i+1].x,touches[i+1].y);
+    
+      
     }
+    
  else{
  
 beginShape();
@@ -42,15 +48,13 @@ vertex(touches[0].x, touches[0].y);
 
 vertex(touches[0].x, touches[0].y);
 endShape(CLOSE);
+   
    // line(touches[i].x,touches[i].y,touches[0].x,touches[0].y);
 }
-                                   
-    
+
    ellipse(touches[i].x,touches[i].y,75); 
   }
 }
 
 //function touchStarted() {
-  
-// fill(random(0,255),random(0,255),random(0,255)); 
-//}
+  //}
