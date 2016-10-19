58a93868177318b920fe8a9607ee2641f944bdec
junso <jsfofana@gmail.com>
2016-10-19 09:31:32 -0500

    zxz

--- cm215/touch/sketch.js
+++ cm215/touch/sketch.js
@@ -14,7 +14,8 @@ function draw() {
   background(255);
 
   for (var i = 0; i < touches.length; i++) {
-    ellipse(touches[i].x, touches[i].y, 80,80);
+    ellipse(touches[i].x, touches[i].y, 80,80
+  fill(random(0, 255), random(0, 255), random(0, 255));
   }
   
   beginShape();
