eabb72567b2a1311e677826e9735f5e1e28330da
junso <jsfofana@gmail.com>
2016-10-19 10:20:49 -0500

    text90

--- cm215/inclinaison/sketch.js
+++ cm215/inclinaison/sketch.js
@@ -6,7 +6,7 @@ function draw() {
   
  background(255);
  
- textSize(40);
+ textSize(90);
  text("Rx: " + floor(rotationX), 100, 100);
  text("Ry: " + floor(rotationY), 100, 150);
  text("Rz: " + floor(rotationZ), 100, 200);
