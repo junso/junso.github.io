cd2041d188c3b1df7e350057557a6d932f1ebaba
junso <jsfofana@gmail.com>
2016-10-26 14:45:37 -0500

    rebond

--- cm215/inclinaison/sketch.js
+++ cm215/inclinaison/sketch.js
@@ -1,20 +1,18 @@
 var ballSize, vY, vX, accX, accY, posX, posY, f;
 
-function setup()
-{
+function setup() {
   createCanvas(windowWidth, windowHeight);
 
   ellipseMode(CENTER);
   ballSize = 50;
   vX = 0;
   vY = 0;
-  posX = windowWidth/2;
-  posY = windowHeight/2;
-  f=0.00001;
+  posX = windowWidth / 2;
+  posY = windowHeight / 2;
+  f = 0.00001;
 }
 
-function draw() 
-{
+function draw() {
   background(255);
   drawBall();
   textSize(45);
@@ -29,18 +27,17 @@ function draw()
   vY += accY;
   posX += vX;
   posY += vY;
-  
-  if ( (posX- ballSize/2) >= windowWidth)
-{
-     vX= -vX;
-    } else{
-      ( (posY - ballSize/2) >= windowHeight)
-   vY= -vY;
-    }
-}
 
+  if ((posX + ballSize / 2) >= windowWidth) {
+    vX = -vX;
+
+  } else {
+    ((posY +ballSize / 2) >= windowHeight)
+    vY = -vY;
+  }
+}
 
 function drawBall() {
   fill(100);
-  ellipse( posX,posY, ballSize, ballSize);
+  ellipse(posX, posY, ballSize, ballSize);
 }
