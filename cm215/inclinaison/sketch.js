7a95af66a3d27d1e9093b8310729fa50f4b35ffc
junso <jsfofana@gmail.com>
2016-10-26 13:55:13 -0500

    retour

--- cm215/inclinaison/sketch.js
+++ cm215/inclinaison/sketch.js
@@ -12,12 +12,6 @@ function draw() {
 
   drawBall();
   textSize(45);
-  accX = Ry;
-  accY = Rx;
-  vitesseX += accX;
-  vitesseY += accY;
-  posX += VitesseX;
-  posY += vitesseY;
 
   text("Rx: " + floor(rotationX), 100, 100);
   text("Ry: " + floor(rotationY), 100, 150);
