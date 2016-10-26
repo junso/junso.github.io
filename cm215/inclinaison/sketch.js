9173fa3bf5bcb9fe77ab89e9211fbf14385107c0
junso <jsfofana@gmail.com>
2016-10-26 14:02:49 -0500

    bbmn,

--- cm215/inclinaison/sketch.js
+++ cm215/inclinaison/sketch.js
@@ -1,10 +1,14 @@
-var ballSize, vY, vZ, accX,accY, posX,posY;
+var ballSize, vY, vZ, accX, accY, posX, posY;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
   ellipseMode(CENTER);
   ballSize = 20;
+  vX = 0;
+  vY = 0;
+  posX = windowWidth/2;
+  posY = windowHeight/    2;
 }
 
 function draw() {
@@ -16,13 +20,13 @@ function draw() {
   text("Rx: " + floor(rotationX), 100, 100);
   text("Ry: " + floor(rotationY), 100, 150);
   text("Rz: " + floor(rotationZ), 100, 200);
-  
-  accX = RotationX;
-  accY =RotationY;
+
+  accX = rotationX;
+  accY = rotationY;
   vX += accX;
   vY += accY;
-  posX += VitesseX;
-  posY += vitesseY;
+  posX += vX;
+  posY += vY;
 }
 
 
