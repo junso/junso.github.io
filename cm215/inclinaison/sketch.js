74f2b5db84d251de8b208237d73e68c32f2aca17
junso <jsfofana@gmail.com>
2016-11-02 13:41:07 -0500

    ffdhdtj

--- cm215/inclinaison/sketch.js
+++ cm215/inclinaison/sketch.js
@@ -1,70 +1,74 @@
 var ballSize, vY, vX, accX, accY, posX, posY, f, bonus, malus, block;
 
 function setup() {
-  
+
   createCanvas(windowWidth, windowHeight);
 
   ellipseMode(CENTER);
   bonus = 22;
   malus = 20;
   block = 20;
-  
+
   ballSize = 50;
   vX = 0;
   vY = 0;
-  posX = windowWidth/2;
-  posY = windowHeight/2;
-  
+  posX = windowWidth / 2;
+  posY = windowHeight / 2;
+
 
 }
 
 function draw() {
-  
+
   background(255);
-  
+
   bomablock();
   drawBall();
-  
-  textSize(45);
 
+  textSize(45);
   text("Rx: " + floor(rotationX), 100, 100);
   text("Ry: " + floor(rotationY), 100, 150);
   text("Rz: " + floor(rotationZ), 100, 200);
-
+  // soit f la friction.
   f = 0.01;
-  
+  //accélération
   accX = rotationY * f;
   accY = rotationX * f;
-  
+  //vitesse
   vX += accX;
   vY += accY;
-  
+  //position
   posX += vX;
   posY += vY;
-  
+  //texte afficher
   text("accX: " + floor(accX), 100, 300);
   text("vX: " + floor(vX), 100, 350);
   text("accY: " + floor(accY), 100, 400);
   text("vY: " + floor(vY), 100, 450);
-  
-  if ((posX + ballSize / 2) >= windowWidth || (posX - ballSize/2) <= 0) {
+  //condition
+  if ((posX + ballSize / 2) >= windowWidth || (posX - ballSize / 2) <= 0) {
     vX = -vX;
   }
-
-  if ((posY + ballSize / 2) >= windowHeight || (posY - ballSize/2) <= 0) {
+  if ((posY + ballSize / 2) >= windowHeight || (posY - ballSize / 2) <= 0) {
     vY = -vY;
   }
 }
 
 function drawBall() {
-  
+
   fill(200, 10, 110);
   ellipse(posX, posY, ballSize, ballSize);
-  
+
 }
-  function bomablock(){
-  fill(200,0,100);
-  ellipse(posX, posY, bonus,bonus)
-  
-  }
- 
+
+function bomablock() {
+  fill(200, 0, 100);
+  ellipse(posX, posY, bonus, bonus)
+
+  fill(100, 0, 100);
+  ellipse(posX, posY, malus, malus)
+
+  fill(250, 0, 10);
+  ellipse(posX, posY, block, block)
+
+}
