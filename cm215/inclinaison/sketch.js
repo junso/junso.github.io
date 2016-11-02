fd4916558d2b4cd846fad102a2e41c465e48b1df
junso <jsfofana@gmail.com>
2016-11-02 14:00:21 -0500

    ryjfgj

--- cm215/inclinaison/sketch.js
+++ cm215/inclinaison/sketch.js
@@ -1,4 +1,4 @@
-var ballSize, vY, vX, accX, accY, posX, posY, f, bonus, malus, block;
+var ballSize, vY, vX, accX, accY, posX, posY, f, bonus, malus, block, bpX, bpY, blpX, blpY, mpX, mpY;
 
 function setup() {
 
@@ -6,8 +6,16 @@ function setup() {
 
   ellipseMode(CENTER);
   bonus = 22;
+  bnpX = random(5,windowWidth);
+  bnpY = random(5,windowHeight);
+
   malus = 20;
+
+ mpX = random(5,windowWidth);
+ mpY = random(5,windowHeight);
   block = 25;
+  blpX = random(5,windowWidth);
+  blpY = random(5,windowHeight);
 
   ballSize = 50;
   vX = 0;
@@ -40,17 +48,22 @@ function draw() {
   //position
   posX += vX;
   posY += vY;
+  
   //texte afficher
   text("accX: " + floor(accX), 100, 300);
   text("vX: " + floor(vX), 100, 350);
   text("accY: " + floor(accY), 100, 400);
   text("vY: " + floor(vY), 100, 450);
-  //condition
+  
+  //condition de rebond sur les bords
   if ((posX + ballSize / 2) >= windowWidth || (posX - ballSize / 2) <= 0) {
     vX = -vX;
   }
   if ((posY + ballSize / 2) >= windowHeight || (posY - ballSize / 2) <= 0) {
     vY = -vY;
+  }
+  if ((bpX + ballSize / 2) >= windowWidth || (bpX - ballSize / 2) <= 0) {
+    vX = -vX;
   }
 }
 
@@ -62,13 +75,13 @@ function drawBall() {
 }
 
 function bomablock() {
-  fill(200, 0, 100);
-  ellipse(posX, posY, bonus, bonus)
+  fill(255, 0, 255);
+  ellipse(bpX, bpY, bonus, bonus);
 
-  fill(100, 0, 100);
-  ellipse(posX, posY, malus, malus)
+  fill(255,0,100);
+  ellipse(blpX, blpY, malus, malus);
 
-  fill(250, 0, 10);
-  ellipse(posX, posY, block, block)
+  fill(0,0,255);
+  ellipse(mpX, mpY, block, block);
 
 }
