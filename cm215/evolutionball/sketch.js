d065a7e3ad10fec8dcc5e571f7edbb1def38e40b
junso <jsfofana@gmail.com>
2016-12-19 17:05:42 -0600

    h

--- cm215/evolutionball/sketch.js
+++ cm215/evolutionball/sketch.js
@@ -1,6 +1,6 @@
   var i, newGame, obstacles = [];
   var type = 0;
-  var iMax, obsX, obsY,img_malus,img_bonus, img_block,img_balle;
+  var iMax, obsX, obsY, img_malus, img_bonus, img_block, img_balle;
   var ballSize, vY, vX, accX, accY, xPos, yPos, bounce, f, colision, bonus, malus, block;
   var score, typeObstacle, level, img, timer, cycle;
   var scoreLevel = [0, 50, 100, 200],
@@ -8,15 +8,16 @@
 
   function preload() {
     img = loadImage("skezako.jpg");
-    img_balle = loadImage("balle.png");
-    img_block = loadImage("block.png");
-    img_bonus = loadImage("bonus.png");
+    //img_balle = loadImage("balle.png");
+    //img_block = loadImage("block.png");
+    //img_bonus = loadImage("bonus.png");
     //img_malus = loadImage("malus.png");
   }
 
   function setup() {
     createCanvas(windowWidth, windowHeight);
     ellipseMode(CENTER);
+    rectMode
     level = 1;
     timer = timerLevel[level];
     newGame = true;
@@ -42,16 +43,16 @@
     for (i = 0; i < iMax; i++) {
       obstacles[i].drawObstacles();
     }
-    fill(255);
+   fill(255);
     textSize(25);
     text("score:" + score, 15, 35);
     text("Time:" + floor(timer / 60), windowWidth / 2 - 120, 35);
-    text("Niveau suivant : 100 points", 15, 70);
+    //text("Niveau suivant : 10 points", 15, 70);
 
     if (timer <= 0) {
       if (score >= scoreLevel[level]) {
         level += 1;
-        newGame = false;
+        newGame = true;
         score = 0;
         timerLevel[level];
       } else {
@@ -65,13 +66,9 @@
   function drawBall() {
 
     if (timer >= 0) {
-     fill(0, 255, 255);
-    
+      fill(0, 255, 255);
+
       ellipse(xPos, yPos, ballSize, ballSize);
-
-
-
-
       f = 0.01; // soit f la friction.
       accX = rotationY * f; //accélérationX
       accY = rotationX * f; //accélérationY
@@ -100,14 +97,9 @@
       }
     }
   }
-
-
-
   //condition de rebond sur les obstacles.
 
   function obst() {
-
-
     if (level == 1) {
       iMax = 3;
     } else if (level == 2) {
@@ -118,13 +110,13 @@
 
     for (i = 0; i < iMax; i++) {
       if (type === 1) {
-        typeobs = "block";
+        typeobs = "bonus";
         type++;
       } else if (type === 2) {
         typeobs = "malus";
         type++;
       } else {
-        typeobs = "bonus";
+        typeobs = "block";
         type = 0;
       }
 
@@ -148,6 +140,7 @@
   }
 
   function testColl() {
+
     for (i = 0; i < iMax; i++) {
       if (dist(xPos, yPos, obstacles[i].xPos, obstacles[i].yPos) <= ballSize / 2 + obstacles[i].size / 2) {
         if (obstacles[i].type === "block") {
@@ -185,13 +178,12 @@
       this.colision = false;
       //this.img = img_bonus;
     } else {
-      this.color = color(0);
+      this.color = color(0, 0, 0);
       //this.img = img_malus;
     }
     this.drawObstacles = function() {
-      
-       //image(this.img, this.xPos, this.yPos, this.size, this.size);
-       ellipse(this.xPos, this.yPos, this.size, this.size);
+      //image(this.img, this.xPos, this.yPos, this.size, this.size);
+      ellipse(this.xPos, this.yPos, this.size, this.size);
     }
   }
 
