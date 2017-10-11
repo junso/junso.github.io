3d697d78237c6a8e2f36aebbadb807967c4af69a
junso <jsfofana@gmail.com>
2017-10-11 10:13:21 -0500

    random_fill

--- CM215/touch/sketch.js
+++ CM215/touch/sketch.js
@@ -12,7 +12,7 @@ function setup() {
 
 function draw() {
   background(255);
-  fill(0);
+  fill(random(0, 255), random(0, 255), random(0, 255));
 
   for (var i = 0; i < touches.length; i++) {
     ellipse(touches[i].x, touches[i].y, 80, 80);
