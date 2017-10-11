3cd397ae3fc92fa1c6f3eae191a8bcb60d7f81ee
junso <jsfofana@gmail.com>
2017-10-11 10:11:29 -0500

    stroke

--- CM215/touch/sketch.js
+++ CM215/touch/sketch.js
@@ -5,27 +5,27 @@ document.ontouchmove = function(event) {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   ellipseMode(CENTER);
-  stroke(0);
+  stroke(10);
   noFill();
   strokeWeight(2);
 }
 
 function draw() {
   background(255);
-fill(0);
- 
+  fill(0);
+
   for (var i = 0; i < touches.length; i++) {
-    ellipse(touches[i].x, touches[i].y, 80,80);
-   
+    ellipse(touches[i].x, touches[i].y, 80, 80);
+
   }
-    fill(random(0, 255), random(0, 255), random(0, 255));
-    
+  fill(random(0, 255), random(0, 255), random(0, 255));
+
   beginShape();
   for (var i = 0; i < touches.length; i++) {
-  
+
     vertex(touches[i].x, touches[i].y);
   }
 
-	endShape(CLOSE);
+  endShape(CLOSE);
 
 }
