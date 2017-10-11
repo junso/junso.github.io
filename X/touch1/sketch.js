986c1966ab171b0619cdde468db17461186d6e51
junso <jsfofana@gmail.com>
2017-10-11 09:43:17 -0500

    modif_additionnel

---
+++ X/touch1/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  rectMode(CENTER);
+  fill(255,0,0);
+}
+
+function draw() 
+{}
+
+  function touchStarted(){
+  background (255);
+  rect(text(touches[0].x,50,50),text(touches[0].y,50,100);)
+  textSize(50);
+  text(touches[0].x,text(touches[0].y,150,150);
+  
+}
