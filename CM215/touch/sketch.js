ba4b8a75e564599ec2452f21fea5868d3fc073a9
junso <jsfofana@gmail.com>
2017-10-11 10:40:33 -0500

    floor

--- CM215/touch/sketch.js
+++ CM215/touch/sketch.js
@@ -15,9 +15,7 @@ function draw() {
   background(255);
   fill(random(0, 255), random(0, 255), random(0, 255));
 
-  textSize(20);
-  text("x: " + floor(),100,100);
-  text("y: " + floor(),100,100);
+ 
 
   for (var i = 0; i < touches.length; i++) {
     ellipse(touches[i].x, touches[i].y, 80, 80);
@@ -29,6 +27,9 @@ function draw() {
   for (var i = 0; i < touches.length; i++) {
 
     vertex(touches[i].x, touches[i].y);
+     textSize(20);
+  text("x: " + floor(),touches[i].x,touches[i].y);
+  text("y: " + floor(),touches[i].x,touches[i].y);
   }
 
   endShape(CLOSE);
