f0442bdf175c462adfbbbddb237f32a50d1a7dcb
junso <jsfofana@gmail.com>
2017-10-18 09:33:45 -0500

    ajout de code

--- CM215/jeu/sketch.js
+++ CM215/jeu/sketch.js
@@ -7,7 +7,7 @@ function setup() {
   vX = 0;
   vY = 0;
   score = 0;
-  ballSize = 50;
+  ballSize = 100;
   bounce = 0.5;
   xPos = windowWidth / 2;
   yPos = windowHeight / 2;
@@ -18,15 +18,49 @@ function draw() {
   background(255);
   drawBall();
   fill(0);
-  textSize(40)
+  
+  textSize(20)
   text("Rx: " + rotationX, 100, 100);
   text("Ry: " + rotationY, 100, 150);
   text("Rz: " + rotationZ, 100, 200);
+  
+   // soit f la friction.
+    f = 0.005;
+    
+    //accélération
+    aX = rotationY * f;
+    aY = rotationX * f;
+    
+    //vitesse
+    vX += aX;
+    vY += aY;
+    
+    //position
+    xPos += vX;
+    yPos += vY;
+    
+/*************************** CONDITIONS DE REBONDISSEMENT *********************/
 
+if ((xPos + ballSize / 2) >= windowWidth) {
+      vX = -vX * bounce;
+      xPos = windowWidth - ballSize / 2;
+    } else if ((xPos - ballSize / 2) <= 0) {
+      vX = -vX * bounce;
+      xPos = ballSize / 2;
+    }
+    //condition de rebond sur les bords du haut et du bas.
+
+    if ((yPos + ballSize / 2) >= windowHeight) {
+      vY = -vY * bounce;
+      yPos = windowHeight - ballSize / 2;
+    } else if ((yPos - ballSize / 2) <= 0) {
+      vY = -vY * bounce;
+      yPos = ballSize / 2;
+    }
 }
 
 function drawBall() {
 
-  fill(150, 50, 0);
+  fill(250, 2550, 0);
   ellipse(xPos, yPos, ballSize, ballSize);
 }
